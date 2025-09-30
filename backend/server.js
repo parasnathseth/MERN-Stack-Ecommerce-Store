ef024999456e3c0fc902ed0b3fa93ffaa7cd8b06
Paras Nath Seth <parasnathseth5@gmail.com>
2025-09-29 23:47:30 -0300

    Setup backend

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.listen(5000, () => {
+  console.log("Server is running on http://localhost:5000...");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
